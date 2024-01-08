69747f16315f96ab68a852b8181821d966c033cd
Alexander Horner <147552108+bbdrip@users.noreply.github.com>
2024-01-08 17:11:43 -0500

    Day 7

---
+++ DAY7-AH/words.py
@@ -0,0 +1,3 @@
+word_list = ["sisters", "condition", "trouble", "tasty", "development", "fang", "carpenter", 
+"violent", "spy", "mass", "fade", "decorous", "large", "language", "ossified", "appear", "crow", 
+"heavy", "heady", "strap", "bubble", "fish", "crabby", "fool"]
